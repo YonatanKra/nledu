6ec9a53b3416ff025331703e08df98a55ab0e3eb
yair.r <yair.r@walkme.com>
2018-08-05 18:31:05 +0300

    Fixed loading.

--- src/data/actions/student.js
+++ src/data/actions/student.js
@@ -1,6 +1,6 @@
 const actions = module.exports = {
 	createStudent(store, props) {
-		const normalizedProps = Object.assign({}, props.data);
+		const normalizedProps = Object.assign({}, props.data || props);
 
 		store.dispatch('CREATE_STUDENT', normalizedProps);
 	}
