53751ead1013e59417a8613b6d364dbb6d12152a
Kfir Zuberi <kfir.z@walkme.com>
2018-08-05 12:46:08 +0300

    fix function

--- functions/index.js
+++ functions/index.js
@@ -17,7 +17,7 @@ exports.saveStory = functions.https.onRequest((req, res) => {
 	// Grab the text parameter.
 	const original = req.query.text;
 	// Push the new message into the Realtime Database using the Firebase Admin SDK.
-	return admin.database().ref('/stories').push({original: original}).then((snapshot) => {
+	return admin.database().ref('/stories').push( req.body).then((snapshot) => {
 		// Redirect with 303 SEE OTHER to the URL of the pushed object in the Firebase console.
 		return res.redirect(303, snapshot.ref.toString());
 	});
