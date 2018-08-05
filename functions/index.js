e1702f9e87b5857ed209be1126060f69956233fd
Kfir Zuberi <kfir.z@walkme.com>
2018-08-05 12:23:48 +0300

    add save story function

--- functions/index.js
+++ functions/index.js
@@ -13,6 +13,16 @@ exports.addMessage = functions.https.onRequest((req, res) => {
 	});
 });
 
+exports.saveStory = functions.https.onRequest((req, res) => {
+	// Grab the text parameter.
+	const original = req.query.text;
+	// Push the new message into the Realtime Database using the Firebase Admin SDK.
+	return admin.database().ref('/stories').push({original: original}).then((snapshot) => {
+		// Redirect with 303 SEE OTHER to the URL of the pushed object in the Firebase console.
+		return res.redirect(303, snapshot.ref.toString());
+	});
+});
+
 exports.makeUppercase = functions.database.ref('/messages/{pushId}/original')
 	.onCreate((snapshot, context) => {
 		// Grab the current value of what was written to the Realtime Database.
